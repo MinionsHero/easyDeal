d0b52345772e3298dd1da31f7bb00694aadd9e29
liguoming <guomingli@pyg168.com>
2020-12-01 19:47:31 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import { PluginFunction } from "vue";
-import NumberInput from "./components/vue2.x/NumberInput.vue";
-import Fluctuation from "./components/vue2.x/Fluctuation.vue";
+import EdNumberInput from "./components/vue2.x/NumberInput.vue";
+import EdFluctuation from "./components/vue2.x/Fluctuation.vue";
 import toFixed from "./utils/toFixed";
+
+export const version = "1.1.2";
+
+export const NumberInput = EdNumberInput;
+export const Fluctuation = EdFluctuation;
 export const utils = { toFixed };
-
-export default {
-  NumberInput,
-  Fluctuation,
-  utils
-};
-
-export const version = "1.1.1";
 
 export const install: PluginFunction<{}> = function (Vue) {
   Vue.use(NumberInput);
